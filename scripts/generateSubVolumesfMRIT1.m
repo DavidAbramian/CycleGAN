08ea6351820dfe794753618b1477eac3ea000eee
Anders Eklund <andek67@harley.ad.liu.se>
2019-10-08 12:52:11 +0200

    Updated sub volume script

--- scripts/generateSubVolumesfMRIT1.m
+++ scripts/generateSubVolumesfMRIT1.m
@@ -4,29 +4,33 @@ clc
 
 addpath('/home/andek67/Research_projects/nifti_matlab')
 
-cubesize = 32; % factor of 4
-cubesizeZ = 32;
+cubesize = 48; % factor of 4
+cubesizeZ = 48;
 
-dataDirectory='/home/andek67/Research_projects/CycleGAN3D/data/fcon1000_128_Beijing/';
-outputDirectory='/home/andek67/Research_projects/CycleGAN3D/data/fcon1000_32cubeshighres_Beijing';
+dataDirectory='/home/andek67/Research_projects/CycleGAN3D/data/fcon1000_64_Beijing_augmented20/';
+outputDirectory='/home/andek67/Research_projects/CycleGAN3D/data/fcon1000_48cubes_Beijing_augmented20';
 
-numberOfSubjects = 198
-numberOfTrainingSubjects = 160;
-numberOfSubvolumesPerSubject = 50;
+originalDataA = dir([dataDirectory '/trainA']);
+originalDataA = originalDataA(3:end);
 
-voxelSize = 1;
+originalDataB = dir([dataDirectory '/trainB']);
+originalDataB = originalDataB(3:end);
+
+numberOfSubvolumesPerSubject = 3;
+
+voxelSize = 2;
 
 subvolumeNumber = 1;
 
-for subject = 1:numberOfTrainingSubjects
+for subject = 1:length(originalDataA)
 
 	subject
 
     % Load full nifti volumes
-    nii = load_nii([dataDirectory '/trainA/Beijing_fMRI_' num2str(subject) '.nii.gz']);
+    nii = load_nii([dataDirectory '/trainA/' originalDataA(subject).name ]);
     fMRI = single(nii.img);
 
-    nii = load_nii([dataDirectory '/trainB/Beijing_T1_' num2str(subject) '.nii.gz' ]);
+    nii = load_nii([dataDirectory '/trainB/' originalDataB(subject).name ]);
     T1 = single(nii.img);
     
     [sy,sx,sz] = size(T1);
@@ -39,16 +43,16 @@ for subject = 1:numberOfTrainingSubjects
         y = max(min( randi(sy,1,1),sy-cubesize/2),cubesize/2+1);
         z = max(min( randi(sz,1,1),sz-cubesizeZ/2),cubesizeZ/2+1);
 
-		% Get 32 cube subvolumes
+		% Get subvolumes
         subvolumefMRI = fMRI( (y-cubesize/2):(y+(cubesize/2-1)) , (x-cubesize/2):(x+(cubesize/2-1)), (z-cubesizeZ/2):(z+(cubesizeZ/2-1)));
 		smallfMRI = make_nii(subvolumefMRI, [voxelSize voxelSize voxelSize], [0 0 0], 16);
         
         subvolumeT1 = T1( (y-cubesize/2):(y+(cubesize/2-1)) , (x-cubesize/2):(x+(cubesize/2-1)), (z-cubesizeZ/2):(z+(cubesizeZ/2-1)));
 		smallT1 = make_nii(subvolumeT1, [voxelSize voxelSize voxelSize], [0 0 0], 16);
 
-        proportionZeros = sum(subvolumefMRI(:) == 0) / (cubesize * cubesize * cubesizeZ);
+        proportionZeros = sum(subvolumefMRI(:) < 10) / (cubesize * cubesize * cubesizeZ);
         
-		if (proportionZeros < 0.7)
+		if (proportionZeros < 0.5)
             
 			save_nii(smallfMRI,[outputDirectory '/trainA/Beijing_fMRI_' num2str(subvolumeNumber) '.nii.gz' ]);
 			save_nii(smallT1,[outputDirectory '/trainB/Beijing_T1_' num2str(subvolumeNumber) '.nii.gz' ]);
